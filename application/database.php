804e652689231602ebca5b10c391b08476a50994
longen0707 <2100703946@qq.com>
2019-04-16 14:40:12 +0800

    加入了auth验证工具类，在index控制器里写了调用的demo，自行挨个访问

--- application/database.php
+++ application/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '39.107.73.171',
     // 数据库名
-    'database'        => '',
+    'database'        => 'auth',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => '123456',
     // 端口
-    'hostport'        => '',
+    'hostport'        => '3306',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
@@ -29,7 +29,7 @@ return [
     // 数据库编码默认采用utf8
     'charset'         => 'utf8',
     // 数据库表前缀
-    'prefix'          => '',
+    'prefix'          => 'think_',
     // 数据库调试模式
     'debug'           => true,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,52 @@
 <?php
 namespace app\index\controller;
 
-class Index
+use think\Controller;
+use think\Loader;
+
+class Index extends Controller
 {
-    public function index()
+    public function index()//当前方法未添加到权限表也未分发权限所以权限验证类默认它为没有权限
     {
         return '这是一个用tp5框架写的auth权限功能模块的demo 可以给一些基础小白借鉴';
     }
+
+    protected $current_action;//用来保存当前的操作url
+    public function _initialize(){
+        Loader::import("org/Auth", EXTEND_PATH);
+        $auth = new \Auth();
+        $this->current_action = request()->module().'/'.request()->controller().'/'.lcfirst(request()->action());//得到当前的操作路径
+        $result = $auth->check($this->current_action, 4);//假设登陆用户的id为4 要验证的操作为当前点击的操作
+        if($result){
+             echo "权限验证成功<br/>";
+        }else{
+            echo "权限验证失败,你没有此权限<br/>";
+        }
+
+    }
+
+    public function add1(){
+        echo "这是add1方法的操作";
+    }
+
+    public function add2(){
+        echo "这是add2方法的操作";
+    }
+
+    public function add3(){
+        echo "这是add3方法的操作";
+    }
+
+    public function add4(){
+        echo "这是add4方法的操作";
+    }
+
+    public function add5(){
+        echo "这是add5方法的操作";
+    }
+
+    public function add6(){
+        echo "这是add6方法的操作，这个操作方法的数据没有在权限表哦，所以不管是谁都会报没有权限";
+    }
+
 }
